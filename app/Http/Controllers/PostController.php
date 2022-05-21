268a1e61564e806448683b7bac5dd51d61211e98
VictorLMgit <victorlimacar@gmail.com>
2022-05-21 15:44:18 -0300

    finish basic API operations without JWT

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,25 +10,39 @@ class PostController extends Controller
     public function __construct(Post $post){
         $this->post = $post;
     }
-    public function index()
-    {
-        // $posts = Post::all(); Método estático
-        $posts = $this->post->all();
-        return $posts;
+
+    public function index(Request $request)
+    {   
+    
+        if($request->has('tag'))
+        {
+            $tag = $request->tag;
+            
+            $posts = $this->post->select('id', 'title', 'author', 'content', 'tags')->where('tags','like',"%$tag%")->get();
+            return response()->json($posts,200);
+        }
+        else
+        {
+            $posts = $this->post->select('id', 'title', 'author', 'content', 'tags')->get();
+            return response()->json($posts,200);
+        }
     }
     
     public function store(Request $request)
     {
+
+        $request->validate($this->post->rules());
         $new_post = $this->post->create($request->all());
-        return $new_post;
+        return response()->json($new_post,201);
     }
 
     // Recebe um inteiro id e pesquiso ele entre meus itens armazenados
     public function show($id)
     {
         $post = $this->post->find($id);
-        if ($post === null){
-            return ['error:'=>'Route not found'];
+        if ($post === null)
+        {
+            return response()->json(['error:'=>'Route not found'],404);
         }
         return $post;
     }
@@ -36,20 +50,36 @@ class PostController extends Controller
     public function update(Request $request, $id)
     {
         $post = $this->post->find($id);
-        if ($post === null){
-            return ['error:'=>'Route not found. update failed'];
+        if ($post === null)
+        {
+            return response()->json(['error:'=>'Route not found. Update failed'],404);
         }
+
+        // Regras parciais para filtrar os campos que serão editados.
+        $partial_rules = array();
+
+        foreach($this->post->rules($id) as $input => $rule)
+        {
+            if(array_key_exists($input, $request->all()))
+            {
+                $partial_rules[$input] = $rule;
+            }
+        }
+
+        $request->validate($partial_rules);
         $post->update($request->all());
-        return $post;
+        $post = $this->post->select('title', 'author', 'content', 'tags','id')->where('id','=',$id)->get();
+        return response()->json($post,200);
     }
 
     public function destroy($id)
     {
         $post = $this->post->find($id);
-        if ($post === null){
-            return ['error:'=>'Route not found. delete failed'];
+        if ($post === null)
+        {
+            return response()->json(['error:'=>'Route not found delete failed'],404);
         }
         $post->delete();
-        return ['msg:' => 'The post has been deleted succefully'];
+        return response()->json(['msg:' => 'The post has been deleted succefully'],200);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,13 @@ class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title','author','content','tags'];
+
+    // post_id = null para quando o id não for passado, as regras serem aplicadas para todas as linhas da tabela
+    public function rules($post_id = null){
+        //unique:tabela,coluna que será trabalhada na tabela,linha que será desconsiderada na busca.
+        return [
+            'title' => 'required|unique:posts,title,'.$post_id, 
+            'author' => 'required',
+        ];
+    }
 }
